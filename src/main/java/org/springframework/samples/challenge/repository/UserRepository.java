0c2f1840906b30aee3502de1087fc608df084908
analuciabolico <analuciabolico.dev@gmail.com>
2019-07-09 16:41:51 -0300

    Modify infra project #3 - Delete unecessary code

--- src/main/java/org/springframework/samples/challenge/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.springframework.samples.challenge.repository;
-
-import org.springframework.dao.DataAccessException;
-import org.springframework.samples.challenge.model.User;
-
-public interface UserRepository {
-
-    void save(User user) throws DataAccessException;
-}
--- src/main/java/org/springframework/samples/challenge/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package org.springframework.samples.challenge.service;
-
-import org.springframework.samples.challenge.model.User;
-
-public interface UserService {
-
-    void saveUser(User user) throws Exception;
-}
