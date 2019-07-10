9c02f6b87821a45ec171fcc6ea402c2866d83d95
rjungsevero <rjungsevero@gmail.com>
2019-07-10 15:28:45 -0300

    #6 Data table valeus

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -62,8 +62,10 @@ INSERT INTO products VALUES (3,
 'Ração Whiskas Sachê Atum para Gatos Adultos 85g', 
 'WHISKAS® Sachê Adulto Sabor Atum ao Molho WHISKAS® sabe o que os gatos naturalmente precisam e amam. Por isso, cada sachê é preparado cuidadosamente com ingredientes de alta qualidade, garantindo os nutrientes que eles precisam todos os dias, e agora com um molho ainda mais encorpado. Disponível em embalagens de 85g, prática, fácil de abrir e que contém a medida certa para uma refeição saudável, completa e balanceada.');
 
-
-
+INSERT INTO sensors VALEUS(1, 'Sensor 1');
+INSERT INTO sensors VALEUS(2, 'Sensor 2');
+INSERT INTO sensors VALEUS(3, 'Sensor 3');
+INSERT INTO sensors VALEUS(4, 'Sensor 4');
 
 
 
