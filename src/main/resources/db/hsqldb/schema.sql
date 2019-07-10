2417411df7bd659402ba0c2ce014b9780a64608c
rjungsevero <rjungsevero@gmail.com>
2019-07-10 16:36:51 -0300

    #6

--- src/main/resources/db/hsqldb/schema.sql
+++ src/main/resources/db/hsqldb/schema.sql
@@ -75,8 +75,7 @@ CREATE INDEX products_name ON products (name);
 CREATE TABLE sensors (
   id   INTEGER IDENTITY PRIMARY KEY,
   name VARCHAR(80),
-  plant VARCHAR(30),
-  humidity VARCHAR(10)  
+  plant VARCHAR(30) 
 );
 CREATE INDEX sensors_name ON sensors (name);
 
