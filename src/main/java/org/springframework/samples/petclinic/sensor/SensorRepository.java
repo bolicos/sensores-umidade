fb7bea5ea0361655448fe80a79f3e523f32a7d03
analuciabolico <analuciabolico.dev@gmail.com>
2019-07-11 13:26:29 -0300

    add new import

--- src/main/java/org/springframework/samples/petclinic/sensor/SensorRepository.java
+++ src/main/java/org/springframework/samples/petclinic/sensor/SensorRepository.java
@@ -2,12 +2,16 @@ package org.springframework.samples.petclinic.sensor;
 
 import java.util.Collection;
 
+import org.springframework.dao.DataAccessException;
 import org.springframework.data.repository.Repository;
 
 public interface SensorRepository extends Repository<Sensor, Integer> {
 
-	Collection<Sensor> findAll();
+	Collection<Sensor> findAll() throws DataAccessException;
 
-	Sensor findById(Integer id);
-
+	Sensor findById(Integer id) throws DataAccessException;
+	
+	void save(Sensor sensor) throws DataAccessException;
+	
+	void delete(Sensor sensor) throws DataAccessException;
 }
