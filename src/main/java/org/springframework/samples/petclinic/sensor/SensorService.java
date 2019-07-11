78c7a2cf5f272b93a2351bb42543a459e4c56215
analuciabolico <analuciabolico.dev@gmail.com>
2019-07-11 13:20:38 -0300

    #14 - Create SensorService

--- src/main/java/org/springframework/samples/petclinic/sensor/SensorService.java
+++ src/main/java/org/springframework/samples/petclinic/sensor/SensorService.java
@@ -7,6 +7,6 @@ import org.springframework.dao.DataAccessException;
 public interface SensorService {
 	Sensor findById(int id) throws DataAccessException;
 	Collection<Sensor> findAll() throws DataAccessException;
-	void saveSensor(Sensor sensor) throws DataAccessException;
-	void deleteSensor(Sensor sensor) throws DataAccessException;
+	public void saveSensor(Sensor sensor) throws DataAccessException;
+	public void deleteSensor(Sensor sensor) throws DataAccessException;
 }
