64e08ded067505c99a1eba9d9714d58cc27c0a94
analuciabolico <analuciabolico.dev@gmail.com>
2019-07-11 10:55:05 -0300

    #08 - Create class to Entity(Sensor)

--- src/main/java/org/springframework/samples/petclinic/sensor/Sensor.java
+++ src/main/java/org/springframework/samples/petclinic/sensor/Sensor.java
@@ -8,29 +8,18 @@ import org.springframework.samples.petclinic.model.NamedEntity;
 @Entity
 @Table(name = "sensors")
 public class Sensor extends NamedEntity {
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	
 	private String plant;
 	private String track;
+	private int humidity;
 	
-	public String getTrack() {
-		return track;
-	}
+	public String getTrack() {return track;}
+	public void setTrack(String track) {this.track = track;}
 
-	public void setTrack(String track) {
-		this.track = track;
-	}
-
-	public String getPlant() {
-		return plant;
-	}
-
-	public void setPlant(String plant) {
-		this.plant = plant;
-	}
-
+	public String getPlant() {return plant;}
+	public void setPlant(String plant) {this.plant = plant;}
+	
+	public int geHumidity() {return humidity;}
+	public void setHumidity(int humidity) {this.humidity = humidity;}
 }
