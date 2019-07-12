d18e64e8f708168a578bd7b2e4f8dd646a263f89
rjungsevero <rjungsevero@gmail.com>
2019-07-12 15:50:57 -0300

    Refactored code

--- src/main/java/org/springframework/samples/petclinic/service/SensorsRestController.java
+++ src/main/java/org/springframework/samples/petclinic/service/SensorsRestController.java
@@ -1,11 +1,10 @@
-package sensors.service;
+package org.springframework.samples.petclinic.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.samples.petclinic.sensor.Sensor;
-import org.springframework.samples.petclinic.service.SensorService;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SensorsRestController {
+	
 	@Autowired
 	private SensorService sensorservice;
 	
