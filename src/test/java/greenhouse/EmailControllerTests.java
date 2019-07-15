7f89c54ffb3cd2b0c9b76ed4114e10b73c0f646f
rjungsevero <rjungsevero@gmail.com>
2019-07-15 13:55:32 -0300

    #Tests class

--- src/test/java/greenhouse/EmailControllerTests.java
+++ src/test/java/greenhouse/EmailControllerTests.java
@@ -2,11 +2,12 @@ package greenhouse;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@SpringBootTest
 public class EmailControllerTests {
-	
 	
 	@Test
     public void showSensorTest() throws Exception {
--- src/test/java/greenhouse/SensorTests.java
+++ src/test/java/greenhouse/SensorTests.java
@@ -2,16 +2,16 @@ package greenhouse;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@SpringBootTest
 public class SensorTests {
 	
 	@Test
     public void testInitCreationForm() throws Exception {
         
     }
-	
-	
 	
 }
