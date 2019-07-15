2fc383b21833b61d1be77623be5e5eaf751ef53c
analuciabolico <analuciabolico.dev@gmail.com>
2019-07-15 18:21:51 -0300

    Edit in file SensorServiceTests

--- src/test/java/greenhouse/SensorServiceTests.java
+++ src/test/java/greenhouse/SensorServiceTests.java
@@ -1,28 +1,24 @@
 package greenhouse;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
-import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 public class SensorServiceTests {
-
-    @LocalServerPort
-    private int port;
 
     @Autowired
     private TestRestTemplate restTemplate;
 
     @Test
     public void greetingShouldReturnDefaultMessage() throws Exception {
-        assertThat(this.restTemplate.getForObject("http://localhost:8080/",
-                String.class)).contains("43242");
+        assertThat(this.restTemplate.getForObject("http://localhost:8080/",String.class))
+        	.isEqualTo("sensorDetails");	
     }
 }
