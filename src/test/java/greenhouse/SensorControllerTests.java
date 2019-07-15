982130ed528900205b88bbfe3c7b1771fe67f18d
rjungsevero <rjungsevero@gmail.com>
2019-07-15 13:49:18 -0300

    #Create Test of class SensorControllerTests

--- src/test/java/greenhouse/SensorControllerTests.java
+++ src/test/java/greenhouse/SensorControllerTests.java
@@ -1,15 +1,22 @@
 package greenhouse;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@SpringBootTest
 public class SensorControllerTests {
 	
+	@Autowired
+	private SensorController sensorController;
 	
-	@Test
-    public void showSensorTest() throws Exception {
-	
+    @Test
+    public void contexLoads() throws Exception {
+        assertThat(sensorController).isNotNull();
     }
 }
