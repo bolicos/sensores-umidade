bfa99515838cf200ceff21e49fb2f8f25dabd1c6
analuciabolico <analuciabolico.dev@gmail.com>
2019-07-16 11:38:26 -0300

    Refactor Tests

--- src/test/java/greenhouse/SensorServiceTests.java
+++ src/test/java/greenhouse/SensorServiceTests.java
@@ -6,19 +6,19 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SensorServiceTests {
 
     @Autowired
-    private TestRestTemplate restTemplate;
+    private RestTemplate restTemplate;
 
     @Test
-    public void greetingShouldReturnDefaultMessage() throws Exception {
-        assertThat(this.restTemplate.getForObject("http://localhost:8080/",String.class))
-        	.isEqualTo("sensorDetails");	
+    public void getDataSensor1Test() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://fundatec.dev.aioria.com.br/api/v1/humidity",String.class))
+        	.isNotNull();
     }
 }
