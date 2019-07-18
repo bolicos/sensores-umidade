822b040d6e67b6b40d015d24cf7689f4794464c6
analuciabolico <analuciabolico.dev@gmail.com>
2019-07-18 13:38:52 -0300

    Create Classe BaseEntity #22

--- src/main/java/greenhouse/models/BaseEntity.java
+++ src/main/java/greenhouse/models/BaseEntity.java
@@ -5,7 +5,9 @@ import java.io.Serializable;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
