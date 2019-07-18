20022aeb67bffbe48ea9bf73b65190e352ba1cac
analuciabolico <analuciabolico.dev@gmail.com>
2019-07-18 13:59:47 -0300

    Create Classe Type #23

--- src/main/java/greenhouse/models/Type.java
+++ src/main/java/greenhouse/models/Type.java
@@ -1,6 +1,14 @@
 package greenhouse.models;
 
-public class Type {
+import javax.persistence.Entity;
+
+@Entity
+public class Type extends BaseEntity{
+	private static final long serialVersionUID = 1L;
+	private String Name;
 	
+	
+	public String getName() {return Name;}
+	public void setName(String name) {Name = name;}
 	
 }
