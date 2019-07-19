067fd2270e9ab344aa3396667f4c967b97c73172
analuciabolico <analuciabolico.dev@gmail.com>
2019-07-19 01:38:05 -0300

    Refactor code #21 -  Change Class Plant

--- src/main/java/greenhouse/models/Plant.java
+++ src/main/java/greenhouse/models/Plant.java
@@ -3,19 +3,34 @@ package greenhouse.models;
 import java.time.LocalDateTime;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@Entity
+@Table(name = "plants")
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Plant extends BaseEntity{
 	private static final long serialVersionUID = 1L;
 	
-	@JsonProperty
+	public Plant(Sensor idSensor, Type type, Integer humidity, String status, Integer trackmin, Integer trackmax,
+			LocalDateTime dataTime) {
+		super();
+		this.idSensor = idSensor;
+		this.type = type;
+		this.humidity = humidity;
+		this.status = status;
+		this.trackmin = trackmin;
+		this.trackmax = trackmax;
+		this.dataTime = dataTime;
+	}
 	
+	@JsonProperty
+	@ManyToOne
 	@JoinColumn(name = "sensor_id")
     private Sensor idSensor;
 	@JsonProperty
@@ -33,24 +48,15 @@ public class Plant extends BaseEntity{
 	@Column(name = "data_time")
 	private LocalDateTime dataTime;
 	
-	
 	@JsonProperty
-	public Integer getId() {return id;}
+	public Sensor getIdSensor() {return idSensor;}
 	@JsonProperty
-	public void setId(Integer id) {this.id = id;}
-	
-	@JsonProperty
-	public Integer getIdsensor() {return idsensor;}
-	@JsonProperty
-	public void setIdsensor(Integer idsensor) {this.idsensor = idsensor;}
+	public void setIdSensor(Sensor idSensor) {this.idSensor = idSensor;}
 	
 	@JsonProperty
 	public Integer getHumidity() {return humidity;}
 	@JsonProperty
 	public void setHumidity(Integer humidity) {this.humidity = humidity;}
-	
-	public String getName() {return name;}
-	public void setName(String name) {this.name = name;}
 	
 	public String getStatus() {return status;}
 	public void setStatus(String status) {this.status = status;}
@@ -63,9 +69,13 @@ public class Plant extends BaseEntity{
 	
 	public LocalDateTime getDataTime() {return dataTime;}
 	public void setDataTime(LocalDateTime dataTime) {this.dataTime = dataTime;}
-	
 
-	
+	@Override
+	public String toString() {
+		return "Plant [idSensor=" + idSensor + ", type=" + type + ", humidity=" + humidity + ", status=" + status
+				+ ", trackmin=" + trackmin + ", trackmax=" + trackmax + ", dataTime=" + dataTime + ", getId()="
+				+ getId() + ", isNew()=" + isNew() + "]";
+	}
 	
 		
 }
