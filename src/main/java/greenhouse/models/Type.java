15374a253763422928d97b0f6eeb3674824a377f
analuciabolico <analuciabolico.dev@gmail.com>
2019-07-19 01:35:15 -0300

    Create Classe Type #23

--- src/main/java/greenhouse/models/Type.java
+++ src/main/java/greenhouse/models/Type.java
@@ -1,9 +1,16 @@
 package greenhouse.models;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "types")
 public class Type extends BaseEntity{
+	public Type(String name) {
+		super();
+		Name = name;
+	}
+
 	private static final long serialVersionUID = 1L;
 	private String Name;
 	
@@ -11,4 +18,8 @@ public class Type extends BaseEntity{
 	public String getName() {return Name;}
 	public void setName(String name) {Name = name;}
 	
+	@Override
+	public String toString() {
+		return "Type [Name=" + Name + ", getId()=" + getId() + ", isNew()=" + isNew() + "]";
+	}
 }
