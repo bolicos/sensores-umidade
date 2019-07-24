6dd41515aebad2be5aea4e9966d1e40b5474c4bc
analuciabolico <analuciabolico.dev@gmail.com>
2019-07-24 17:27:36 -0300

    Update PlantRepository

--- src/main/java/greenhouse/repositories/PlantRepository.java
+++ src/main/java/greenhouse/repositories/PlantRepository.java
@@ -11,7 +11,7 @@ import greenhouse.models.Plant;
 
 public interface PlantRepository extends Repository<Plant, Integer>{
 	Collection<Plant> findAll() throws DataAccessException;
-	Plant findById(Integer id) throws DataAccessException;
+	Plant findById(@Param("id") Integer id) throws DataAccessException;
 	
 	@Query("SELECT sen FROM Plant sen WHERE sen.idSensor.id = ?1")
 	Collection<Plant> findBySensor(@Param("id") Integer id);
