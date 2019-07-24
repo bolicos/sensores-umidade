18a3722156f0feb348a00385e15af2f44930fb7b
analuciabolico <analuciabolico.dev@gmail.com>
2019-07-24 17:16:27 -0300

    Update PlantController

--- src/main/java/greenhouse/controllers/PlantController.java
+++ src/main/java/greenhouse/controllers/PlantController.java
@@ -1,12 +1,12 @@
 package greenhouse.controllers;
 
 import java.util.Collection;
+import java.util.Map;
 
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,7 +23,7 @@ import greenhouse.repositories.TypeRepository;
 public class PlantController {
 	
 	private static final String VIEWS_PLANT_CREATE_OR_UPDATE_FORM = "plant/createOrUpdatePlantForm";
-	private static final String VIEWS_SENSOR_CREATE_OR_UPDATE_FORM = "plant/createOrUpdateSensortForm";
+	private static final String VIEWS_SENSOR_CREATE_OR_UPDATE_FORM = "sensor/createOrUpdateSensortForm";
 	private PlantRepository plants;
 	private SensorRepository sensors;
 	private TypeRepository types;
@@ -75,17 +75,41 @@ public class PlantController {
 		Collection<Plant> sensorDetails = plants.findBySensor(sensorId);	
 		model.addAttribute("sensor", sensorDetails);
 		return "sensor/sensorDetails";
-	}
-	
-	
-	@PostMapping("/plants/{plantId}/edit")
-    public String createTypePlanty(@Valid Type type, BindingResult result, ModelMap model) {
+	}	
+//------------------------------------------------------------------------------------------CREATE PLANT TYPE
+	@PostMapping("/plants/new")
+    public String processcreateTypePlant(@Valid Type type, BindingResult result) {
         if (result.hasErrors()) {
-            model.put("type", type);
             return VIEWS_PLANT_CREATE_OR_UPDATE_FORM;
         } else {
             this.types.save(type);
             return "redirect:/plants/{plantId}";
         }
     }
+	
+	@GetMapping("/plants/new")
+    public String initcreateTypePlant(Map<String, Object> model) {
+		Type type = new Type();
+        model.put("type", type);
+        return VIEWS_PLANT_CREATE_OR_UPDATE_FORM;
+    }
+//------------------------------------------------------------------------------------------===============
+//------------------------------------------------------------------------------------------CREATE SENSOR
+	@PostMapping("/sensors/new")
+    public String processcreateSensor(@Valid Sensor sensor, BindingResult result) {
+        if (result.hasErrors()) {
+            return VIEWS_SENSOR_CREATE_OR_UPDATE_FORM;
+        } else {
+            this.sensors.save(sensor);
+            return "redirect:/sensors/{sensorId}";
+        }
+    }
+	
+	@GetMapping("/sensors/new")
+    public String initcreateSensor(Map<String, Object> model) {
+		Sensor sensor = new Sensor();
+        model.put("sensor", sensor);
+        return VIEWS_SENSOR_CREATE_OR_UPDATE_FORM;
+    }
+//------------------------------------------------------------------------------------------===============
 }
