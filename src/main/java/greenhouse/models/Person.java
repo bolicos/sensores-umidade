89c97c11dc43c24d50d628d9748a8cfd8934c00b
analuciabolico <analuciabolico.dev@gmail.com>
2019-07-24 17:57:05 -0300

    Create model Person - #31

---
+++ src/main/java/greenhouse/models/Person.java
@@ -0,0 +1,25 @@
+package greenhouse.models;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotEmpty;
+
+@MappedSuperclass
+public class Person extends BaseEntity{
+	private static final long serialVersionUID = 1L;
+	
+	@Column(name = "first_name")
+    @NotEmpty
+    private String firstName;
+
+    @Column(name = "last_name")
+    @NotEmpty
+    private String lastName;
+
+    public String getFirstName() {return this.firstName;}
+    public void setFirstName(String firstName) {this.firstName = firstName;}
+
+    public String getLastName() {return this.lastName;}
+    public void setLastName(String lastName) {this.lastName = lastName;}
+    
+}
