45d71feb4a25c0b0d7bd80fd519d22a2b8658fd6
analuciabolico <analuciabolico.dev@gmail.com>
2019-07-25 18:27:38 -0300

    Update Repositories #36

--- src/main/java/greenhouse/repositories/UserRepository.java
+++ src/main/java/greenhouse/repositories/UserRepository.java
@@ -10,8 +10,8 @@ import greenhouse.models.User;
 
 public interface UserRepository extends Repository<User, Integer>{
 	Collection<User> findAll() throws DataAccessException;
-	Collection<User> findByFirstName(@Param("first_name") String name) throws DataAccessException;
-	User findById(@Param("userName") String userName) throws DataAccessException;
+	Collection<User> findByFirstName(@Param("first_name") String firstName) throws DataAccessException;
+	User findByUserName(@Param("userName") String userName) throws DataAccessException;
 	public void save(User user) throws DataAccessException;
 	public void delete(User user) throws DataAccessException;
 
