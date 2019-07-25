c7f07fee44de25aeed84c57b3de90c6895cb1c72
analuciabolico <analuciabolico.dev@gmail.com>
2019-07-25 18:15:37 -0300

    Update Sensor - #33

--- src/main/java/greenhouse/models/Sensor.java
+++ src/main/java/greenhouse/models/Sensor.java
@@ -1,7 +1,12 @@
 package greenhouse.models;
 
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -9,23 +14,34 @@ import javax.persistence.Table;
 public class Sensor extends BaseEntity{
 	private static final long serialVersionUID = 1L;
 	
-	public Sensor() {
-		super();
-	}
+	public Sensor() {super();}
+	
+	@ManyToOne
+    @JoinColumn(name = "username")
+    private User user;
+	
+	@ManyToOne
+    @JoinColumn(name = "plant_id")
+    private Plant plant;
+	
+	@OneToMany
+    private Set<Read> reads;
 
 	@Column(name = "name")
 	private String name;
 	
-	public Sensor(String name) {
-		super();
-		this.name = name;
-	}
-	
 	public String getName() {return name;}
 	public void setName(String name) {this.name = name;}
 	
-	@Override
-	public String toString() {
-		return "Sensor [name=" + name + ", getId()=" + getId() + ", isNew()=" + isNew() + "]";
-	}
+	public User getUser() {return user;}
+	public void setUser(User user) {this.user = user;}
+	
+	public Plant getPlant() {return plant;}
+	public void setPlant(Plant plant) {this.plant = plant;}
+	
+	public Set<Read> getReads() {return reads;}
+	public void setReads(Set<Read> reads) {this.reads = reads;}
+	
+	
+	
 }
