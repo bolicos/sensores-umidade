3b9dbea4d48cd63360568e5560566caeef9a147d
analuciabolico <analuciabolico.dev@gmail.com>
2019-07-25 18:28:00 -0300

    Update Controllers #35

--- src/main/java/greenhouse/controllers/UserController.java
+++ src/main/java/greenhouse/controllers/UserController.java
@@ -34,49 +34,49 @@ public class UserController {
     }
 //------------------------------------------------------------------------
 //-------------------------------------NEW USER-------------------------------------------
-    @GetMapping("/users/new")
-    public String initCreationForm(Map<String, Object> model) {
-        User user = new User();
-        model.put("user", user);
-        return VIEWS_USER_CREATE_OR_UPDATE_FORM;
-    }
-
-    @PostMapping("/users/new")
-    public String processCreationForm(@Valid User user, BindingResult result) {
-        if (result.hasErrors()) {
-            return VIEWS_USER_CREATE_OR_UPDATE_FORM;
-        } else {
-            this.users.save(user);
-            return "redirect:/users/" + user.getUserName();
-        }
-    }
+//    @GetMapping("/users/new")
+//    public String initCreationForm(Map<String, Object> model) {
+//        User user = new User();
+//        model.put("user", user);
+//        return VIEWS_USER_CREATE_OR_UPDATE_FORM;
+//    }
+//
+//    @PostMapping("/users/new")
+//    public String processCreationForm(@Valid User user, BindingResult result) {
+//        if (result.hasErrors()) {
+//            return VIEWS_USER_CREATE_OR_UPDATE_FORM;
+//        } else {
+//            this.users.save(user);
+//            return "redirect:/users/" + user.getUserName();
+//        }
+//    }
 //--------------------------------------------------------------------------------------
 //-------------------------------------EDIT USER----------------------------------------
-    @GetMapping("/users/{userName}/edit")
-    public String initUpdateUserForm(@PathVariable("userName") String userName, Model model) {
-    	User user = this.users.findById(userName);
-        model.addAttribute(user);
-        return VIEWS_USER_CREATE_OR_UPDATE_FORM;
-    }
-
-    @PostMapping("/users/{userName}/edit")
-    public String processUpdateOwnerForm(@Valid User user, BindingResult result, @PathVariable("userId") String userName) {
-        if (result.hasErrors()) {
-            return VIEWS_USER_CREATE_OR_UPDATE_FORM;
-        } else {
-        	user.setUserName(userName);
-            this.users.save(user);
-            return "redirect:/users/{userId}";
-        }
-    }
+//    @GetMapping("/users/{userName}/edit")
+//    public String initUpdateUserForm(@PathVariable("userName") String userName, Model model) {
+//    	User user = this.users.findByUserName(userName);
+//        model.addAttribute(user);
+//        return VIEWS_USER_CREATE_OR_UPDATE_FORM;
+//    }
+//
+//    @PostMapping("/users/{userName}/edit")
+//    public String processUpdateOwnerForm(@Valid User user, BindingResult result, @PathVariable("userId") String userName) {
+//        if (result.hasErrors()) {
+//            return VIEWS_USER_CREATE_OR_UPDATE_FORM;
+//        } else {
+//        	user.setUserName(userName);
+//            this.users.save(user);
+//            return "redirect:/users/{userId}";
+//        }
+//    }
     
     
     
     
-    @GetMapping("/users/{userName}")
-    public ModelAndView showUser(@PathVariable("userId") String userName) {
-        ModelAndView mav = new ModelAndView("users/userDetails");
-        mav.addObject(this.users.findById(userName));
-        return mav;
-    }
+//    @GetMapping("/users/{userName}")
+//    public ModelAndView showUser(@PathVariable("userId") String userName) {
+//        ModelAndView mav = new ModelAndView("users/userDetails");
+//        mav.addObject(this.users.findById(userName));
+//        return mav;
+//    }
 }
