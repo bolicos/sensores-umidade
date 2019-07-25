e429899747f6c9dc65ee1f167295d75ea885f56b
analuciabolico <analuciabolico.dev@gmail.com>
2019-07-25 18:15:05 -0300

    Update Plant - #33

--- src/main/java/greenhouse/models/Plant.java
+++ src/main/java/greenhouse/models/Plant.java
@@ -1,77 +1,34 @@
 package greenhouse.models;
 
-import java.time.LocalDateTime;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity
 @Table(name = "plants")
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class Plant extends BaseEntity{
 	private static final long serialVersionUID = 1L;
 	
 	public Plant() {super();}
 	
-	@JsonProperty
-	@ManyToOne
-	@JoinColumn(name = "sensor_id")
-    private Sensor idSensor;
-	@JsonProperty
-	@ManyToOne
-	@JoinColumn(name = "type_id")
-    private Type type;
-	@JsonProperty
-	@Column(name = "humidity")
-	private Integer humidity;
-	@Column(name = "status")
-    private String status;
+	@Column(name = "type_id")
+	private Type type;
 	
-	@Column(name = "data_time")
-	private LocalDateTime dataTime;
+	@OneToMany
+    private Set<Sensor> reads;
 	
-	public Plant(Sensor idSensor, Type type, Integer humidity, String status, LocalDateTime dataTime) {
-		super();
-		this.idSensor = idSensor;
-		this.type = type;
-		this.humidity = humidity;
-		this.status = status;
-		this.dataTime = dataTime;
-	}
-	
-	@JsonProperty
-	public Sensor getIdSensor() {return idSensor;}
-	@JsonProperty
-	public void setIdSensor(Sensor idSensor) {this.idSensor = idSensor;}
-	
-	@JsonProperty
 	public Type getType() {return type;}
-	@JsonProperty
 	public void setType(Type type) {this.type = type;}
-	
-	@JsonProperty
-	public Integer getHumidity() {return humidity;}
-	@JsonProperty
-	public void setHumidity(Integer humidity) {this.humidity = humidity;}
-	
-	public String getStatus() {return status;}
-	public void setStatus(String status) {this.status = status;}
-	
-	@JsonProperty
-	public LocalDateTime getDataTime() {return dataTime;}
-	@JsonProperty
-	public void setDataTime(LocalDateTime dataTime) {this.dataTime = dataTime;}
+
+	public Set<Sensor> getReads() {return reads;}
+	public void setReads(Set<Sensor> reads) {this.reads = reads;}
 
 	@Override
 	public String toString() {
-		return "Plant [idSensor=" + idSensor + ", type=" + type + ", humidity=" + humidity + ", status=" + status
-				+ ", dataTime=" + dataTime + ", getId()=" + getId() + ", isNew()=" + isNew() + "]";
+		return "Plant [type=" + type + ", reads=" + reads + ", getId()=" + getId() + ", isNew()=" + isNew() + "]";
 	}
-		
+
 }
