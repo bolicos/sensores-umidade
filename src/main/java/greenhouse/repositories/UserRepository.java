2b750082788a25166e6c4e32c22559e6a65c2c02
analuciabolico <analuciabolico.dev@gmail.com>
2019-07-26 12:07:51 -0300

    Update Repository User

--- src/main/java/greenhouse/repositories/UserRepository.java
+++ src/main/java/greenhouse/repositories/UserRepository.java
@@ -12,6 +12,7 @@ public interface UserRepository extends Repository<User, Integer>{
 	Collection<User> findAll() throws DataAccessException;
 	Collection<User> findByFirstName(@Param("first_name") String firstName) throws DataAccessException;
 	User findByUserName(@Param("userName") String userName) throws DataAccessException;
+	User findByPassword(@Param("password") String password) throws DataAccessException;
 	public void save(User user) throws DataAccessException;
 	public void delete(User user) throws DataAccessException;
 
