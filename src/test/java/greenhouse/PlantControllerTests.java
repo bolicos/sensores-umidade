bb49be131a6ddc584a9ff963bff654f8aeccf414
analuciabolico <analuciabolico.dev@gmail.com>
2019-09-21 21:36:34 -0300

    #38

--- src/test/java/greenhouse/PlantControllerTests.java
+++ src/test/java/greenhouse/PlantControllerTests.java
@@ -4,51 +4,25 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-
-import greenhouse.models.Read;
-import greenhouse.models.Sensor;
-import greenhouse.models.Type;
-import greenhouse.repositories.PlantRepository;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
 @AutoConfigureMockMvc
-@WithMockUser(roles = "USER_ADMIN'")
+@WithMockUser(roles = "USER_ADMIN")
 public class PlantControllerTests {
-	
-	private static final Integer TEST_PLANT_ID = 1;
-	private static final Integer TEST_TYPE_ID = 1;
-	private static final Integer TEST_SENSOR_ID = 1;
 	
 	@Autowired
 	private MockMvc mockMvc;
 	
-	@MockBean
-	private PlantRepository plants;
-	
-	private Read read;
-	private Type type;
-	private Sensor sensor;
-	
-	@Before
-	public void setup() {
-		this.read = new Read();
-		this.read.setId(TEST_PLANT_ID);
-		//this.plant.setIdSensor(TEST_SENSOR_ID);
-		//this.plant.setType(TEST_TYPE_ID);
-		this.read.setHumidity(70);
-	}
 	@Test
 	public void welcome() throws Exception {
 		mockMvc.perform(get("/"))
