97fe7d95126560602e3a7ead38a90bfe4e6e6f26
analuciabolico <analuciabolico.dev@gmail.com>
2019-09-30 20:39:55 -0300

    security

--- src/main/java/greenhouse/Application.java
+++ src/main/java/greenhouse/Application.java
@@ -2,21 +2,21 @@ package greenhouse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String args[]) {
 		SpringApplication.run(Application.class);
-		System.out.print(new BCryptPasswordEncoder().encode("senh@maria"));
-		System.out.println("\n");
-		System.out.print(new BCryptPasswordEncoder().encode("senh@joao"));
-		System.out.println("\n");
-		System.out.print(new BCryptPasswordEncoder().encode("senh@ariel"));
-		System.out.println("\n");
-		System.out.print(new BCryptPasswordEncoder().encode("senh@admin"));
-		System.out.println("\n");
+		
+//		System.out.print(new BCryptPasswordEncoder().encode("senh@maria"));
+//		System.out.println("\n");
+//		System.out.print(new BCryptPasswordEncoder().encode("senh@joao"));
+//		System.out.println("\n");
+//		System.out.print(new BCryptPasswordEncoder().encode("senh@ariel"));
+//		System.out.println("\n");
+//		System.out.print(new BCryptPasswordEncoder().encode("senh@admin"));
+//		System.out.println("\n");
 	}
 	
 	
